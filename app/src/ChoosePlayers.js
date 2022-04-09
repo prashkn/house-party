a5019c2a3487e8daaa785886738229a12af96524
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-09 00:02:58 -0500

    add layout for deck selection and navigation

--- app/src/ChoosePlayers.js
+++ app/src/ChoosePlayers.js
@@ -1,47 +1,58 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
-import { Button } from "react-native-ios-kit";
+import masterstyles from "./MasterStyle";
+import { Icon } from "@rneui/themed";
+import DeckCard from "./DeckCard";
+import { data } from "./data";
 
-export default function ChoosePlayers() {
+export default function ChoosePlayers({ navigation }) {
   const [text, onChangeText] = React.useState(
-    "Samyak, Garima, Ayesha, Sowmya, Trisha..."
+    "Samyak, Garima, Ayesha, Sowmya, Trisha"
   );
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Who's playing?</Text>
-      <TextInput
-        clearTextOnFocus
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
+    <View style={masterstyles.container}>
+      <Text style={masterstyles.header}>Who's playing?</Text>
+
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          clearTextOnFocus
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <Icon
+          name="arrow-right-box"
+          type="material-community"
+          color="#475DFA"
+          style={{ marginTop: 108, marginLeft: 15 }}
+          onPress={() =>
+            navigation.navigate("Game", {
+              players: text,
+            })
+          }
+        />
+      </View>
+      <View style={{ flexDirection: "row", marginLeft: 40 }}>
+        {data.map((deck) => (
+          <DeckCard name={deck.name} description={deck.description} />
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FAAD54",
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-  },
-  header: {
-    marginTop: 110,
-    marginLeft: 40,
-    fontSize: 30,
-    color: "#475DFA",
-    fontWeight: "bold",
-  },
   input: {
     height: 40,
     alignSelf: "center",
-    marginTop: 80,
+    marginTop: 100,
+    marginLeft: 40,
     backgroundColor: "white",
-    width: "80%",
+    width: "68%",
     borderColor: "#475DFA",
     borderWidth: 2,
     borderRadius: 10,
     padding: 10,
+    color: "gray",
   },
 });
