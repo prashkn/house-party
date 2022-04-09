b2ac6e804144217d8442e7cfdb46069dd605a9a9
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-09 00:04:52 -0500

    created component for deck card

---
+++ app/src/DeckCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function DeckCard({ name, description }) {
+  const [selected, setSelected] = React.useState(false);
+  const styles = StyleSheet.create({
+    card: {
+      height: 250,
+      width: "50%",
+      backgroundColor: "#83A9E3",
+      borderRadius: 30,
+      marginTop: 40,
+      borderWidth: selected ? 5 : 0,
+      borderColor: "#336BC3",
+      marginRight: 15,
+    },
+    text: {
+      marginTop: "25%",
+      textAlign: "center",
+      fontSize: 16,
+      fontWeight: "bold",
+      color: "#336BC3",
+    },
+  });
+  return (
+    <View style={styles.card} onTouchStart={() => setSelected(!selected)}>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{description}</Text>
+    </View>
+  );
+}
