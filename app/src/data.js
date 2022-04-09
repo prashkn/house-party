9b269ffa7961e853bc47074dec9e9806ddf73839
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-09 00:03:29 -0500

    created data that contains deck info

---
+++ app/src/data.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const data = [
+  {
+    name: "Classic",
+    description: "Classic description",
+    questions: ["question 1", "question 2", "question 3", "question 4"],
+  },
+  {
+    name: "NSFW",
+    description: "NSFW description",
+    questions: ["question 1", "question ", "question 3", "question 4"],
+  },
+];
