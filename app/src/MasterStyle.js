2bafca884971082a004760a53404a03e81c5ff33
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-09 00:05:48 -0500

    create master styling file

---
+++ app/src/MasterStyle.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFD392",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  header: {
+    marginTop: 110,
+    marginLeft: 40,
+    fontSize: 30,
+    color: "#475DFA",
+    fontWeight: "bold",
+  },
+  actionbtn: {
+    backgroundColor: "#336BC3",
+    marginBottom: 10,
+    width: "80%",
+  },
+  actionbtnfont: {
+    color: "white",
+    textAlign: "center",
+    fontSize: "16px",
+    fontSize: 16,
+  },
+});
