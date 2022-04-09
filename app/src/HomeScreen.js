9dc755cc14447cdc1a1585224f8626781be35103
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-09 00:05:38 -0500

    pulled master styling out of home screen file

--- app/src/HomeScreen.js
+++ app/src/HomeScreen.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Button } from "react-native-ios-kit";
+import masterstyles from "./MasterStyle";
 
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>House Party</Text>
+      <Image
+        style={styles.logo}
+        source={require("../assets/house_party.png")}
+      />
       <Button
         inline
         rounded
-        style={styles.actionbtn}
+        style={masterstyles.actionbtn}
         onPress={() => navigation.navigate("ChoosePlayers")}
       >
-        <Text style={styles.actionbtnfont}>Quick Play</Text>
+        <Text style={masterstyles.actionbtnfont}>Quick Play</Text>
       </Button>
       <Button
         inline
         rounded
-        style={styles.actionbtn}
-        onPress={() => navigation.navigate("ChooseDeck")}
+        style={masterstyles.actionbtn}
+        onPress={() => navigation.navigate("CreateDeck")}
       >
-        <Text style={styles.actionbtnfont}>Choose Deck</Text>
+        <Text style={masterstyles.actionbtnfont}>Create a new Deck</Text>
       </Button>
     </View>
   );
@@ -29,26 +33,12 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FAAD54",
+    backgroundColor: "#FFD392",
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    marginTop: 100,
-    paddingBottom: 180,
-    fontSize: 30,
-    color: "#475DFA",
-    fontWeight: "bold",
-  },
-  actionbtn: {
-    backgroundColor: "#475DFA",
-    marginBottom: 10,
-    width: "80%",
-  },
-  actionbtnfont: {
-    color: "white",
-    textAlign: "center",
-    fontSize: "16px",
-    fontSize: 16,
+  logo: {
+    width: 300,
+    height: 300,
   },
 });
