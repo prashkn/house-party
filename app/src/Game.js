44b67cd42d4960a51a7c1cdc078db426d3a95cfe
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-10 18:34:24 -0500

    added game over condition and created random qstns

--- app/src/Game.js
+++ app/src/Game.js
@@ -1,34 +1,80 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import * as Animatable from "react-native-animatable";
 import masterstyles from "./MasterStyle";
+import { Button } from "react-native-ios-kit";
 import { data } from "./data";
 
 export default function Game({ route, navigation }) {
+  const maxQuestionstoAsk = 40;
   const { players, deck } = route.params;
 
-  const playerList = players.split(",");
-  playerList.forEach((player) => player.trim());
+  const playerList = [];
+  players.split(",").forEach((player) => playerList.push(player.trim()));
 
   const promptList = [];
   data.forEach((deck) => {
     deck.questions.forEach((question) => promptList.push(question));
   });
 
+  function getRandomPlayer() {
+    const randNum = Math.floor(Math.random() * playerList.length);
+    return playerList[randNum];
+  }
+
+  /*
+  deck.forEach((question) => {
+    promptList.push(question);
+  });
+  */
+
   const [promptedText, setPromptedText] = React.useState(promptList[0]);
   const [chosenAnimation, setChosenAnimation] = React.useState("bounce");
+  const [amntOfQuestions, setAmntOfQuestions] = React.useState(1);
 
   const generateNewPrompt = () => {
+    setAmntOfQuestions(amntOfQuestions + 1);
+
     var randomNum = Math.floor(Math.random() * promptList.length);
-    setPromptedText(promptList[randomNum]);
+    const question = promptList[randomNum];
+
+    let prompted = "";
+    question.forEach((phrase, idx) => {
+      if (idx != question.length - 1) {
+        prompted += phrase + getRandomPlayer();
+      } else {
+        prompted += phrase;
+      }
+    });
+
+    setPromptedText(prompted);
     setChosenAnimation("bounce");
   };
 
   return (
     <View style={styles.container} onTouchStart={generateNewPrompt}>
-      <Animatable.Text animation={chosenAnimation} style={styles.prompt}>
-        {promptedText}
-      </Animatable.Text>
+      {amntOfQuestions < maxQuestionstoAsk && (
+        <Animatable.Text animation={chosenAnimation} style={styles.prompt}>
+          {promptedText}
+        </Animatable.Text>
+      )}
+      {amntOfQuestions >= maxQuestionstoAsk && (
+        <Animatable.Text animation={chosenAnimation} style={styles.prompt}>
+          Game Over! {"\n"} Play Again?
+          <Button
+            inline
+            rounded
+            style={[masterstyles.actionbtn, styles.btn]}
+            onPress={() =>
+              navigation.navigate("ChoosePlayers", {
+                players: playerList.join(", "),
+              })
+            }
+          >
+            <Text style={masterstyles.actionbtnfont}>Create a new Deck</Text>
+          </Button>
+        </Animatable.Text>
+      )}
     </View>
   );
 }
@@ -44,5 +90,11 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     color: "#475DFA",
+    width: "80%",
+    textAlign: "center",
+  },
+  btn: {
+    marginLeft: 40,
+    marginTop: 30,
   },
 });
