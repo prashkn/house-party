1a186ae09ca37c1794c887818c4d0723b0a86ca8
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-10 18:33:25 -0500

    remove deck display for now and added route param

--- app/src/ChoosePlayers.js
+++ app/src/ChoosePlayers.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import masterstyles from "./MasterStyle";
-import { Icon } from "@rneui/themed";
+import { Button } from "react-native-ios-kit";
 import DeckCard from "./DeckCard";
 import { data } from "./data";
 
-export default function ChoosePlayers({ navigation }) {
-  const [text, onChangeText] = React.useState(
-    "Samyak, Garima, Ayesha, Sowmya, Trisha"
-  );
+function startGame(navigation, text) {
+  navigation.navigate("Game", {
+    players: text,
+    deck: "txt",
+  });
+}
+
+export default function ChoosePlayers({ route, navigation }) {
+  const { players } = route.params;
+
+  const placeHolder =
+    players == "" ? "Samyak, Garima, Ayesha, Sowmya, Trisha" : players;
+  const [text, onChangeText] = React.useState(placeHolder);
+
   return (
     <View style={masterstyles.container}>
       <Text style={masterstyles.header}>Who's playing?</Text>
-
       <View style={{ flexDirection: "row" }}>
         <TextInput
           clearTextOnFocus
@@ -20,6 +29,7 @@ export default function ChoosePlayers({ navigation }) {
           onChangeText={onChangeText}
           value={text}
         />
+        {/*
         <Icon
           name="arrow-right-box"
           type="material-community"
@@ -31,12 +41,23 @@ export default function ChoosePlayers({ navigation }) {
             })
           }
         />
+        */}
       </View>
+      {/*
       <View style={{ flexDirection: "row", marginLeft: 40 }}>
         {data.map((deck) => (
           <DeckCard name={deck.name} description={deck.description} />
         ))}
       </View>
+        */}
+      <Button
+        inline
+        rounded
+        style={[masterstyles.actionbtn, styles.btn]}
+        onPress={() => startGame(navigation, text)}
+      >
+        <Text style={masterstyles.actionbtnfont}>Start Game</Text>
+      </Button>
     </View>
   );
 }
@@ -45,14 +66,18 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     alignSelf: "center",
-    marginTop: 100,
+    marginTop: 80,
     marginLeft: 40,
     backgroundColor: "white",
-    width: "68%",
+    width: "80%",
     borderColor: "#475DFA",
     borderWidth: 2,
     borderRadius: 10,
     padding: 10,
     color: "gray",
+  },
+  btn: {
+    marginLeft: 40,
+    marginTop: 20,
   },
 });
