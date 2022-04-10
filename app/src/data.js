1a33986b3f9c2956114f02f8c419ec4f1520e6fe
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-10 18:33:34 -0500

    changed questions to include players

--- app/src/data.js
+++ app/src/data.js
@@ -4,11 +4,14 @@ export const data = [
   {
     name: "Classic",
     description: "Classic description",
-    questions: ["question 1", "question 2", "question 3", "question 4"],
+    questions: [["Question for ", ". Hello"], [""], [""], [""], [""]],
   },
   {
     name: "NSFW",
     description: "NSFW description",
-    questions: ["question 1", "question ", "question 3", "question 4"],
+    questions: [
+      ["Not a question for ", ". Hello"],
+      ["If you are not ", ". Hello"],
+    ],
   },
 ];
