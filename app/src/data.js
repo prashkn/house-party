6489d4a3b8cadf7f5e1cea6e92833269eb24711c
Prashant Naganaboyina <pnaganab@gmail.com>
2022-04-16 13:09:32 -0500

    made more questions

--- app/src/data.js
+++ app/src/data.js
@@ -4,14 +4,69 @@ export const data = [
   {
     name: "Classic",
     description: "Classic Deck description",
-    questions: [["Example Question for ", ""]],
+    questions: [
+      [
+        "",
+        ", if you ever made out with anyone in this room, take 3 penalties. If not, give out 1 penalty.",
+      ],
+      [
+        "",
+        ", if you ever drank more than 3 days in a row, take a penalty. If not, give out 3 penalties.",
+      ],
+      [
+        "Go around the room and list out a type of drink. The first player who repeats a word or can't think of one takes 3 penalties.",
+      ],
+      [
+        "",
+        ": Between ",
+        " and ",
+        ", who would you trust driving your car? Loser takes 3 penalties.",
+      ],
+      [
+        "",
+        ", if you can name the song playing right now, give out as many penalities as players in this room.",
+      ],
+      [
+        "",
+        ", for the rest of the game, you are in control of what ",
+        " is drinking. If they don't agree, they take a maximum penalty.",
+      ],
+      [
+        "Whoever was the last one to arrive, take as many penalties as ",
+        " finds fit.",
+      ],
+    ],
   },
   {
     name: "UIUC",
     description: "UIUC Deck description",
     questions: [
-      ["UIUC Example Question for ", "."],
-      ["Another Example Prompt from the UIUC Deck for ", "."],
+      [
+        "",
+        ", if you ever blacked out at KAMS, take 2 penalties. If not, give out 2 penalties.",
+      ],
+      [
+        "",
+        ", if you ever blacked out at Lion, take 2 penalties. If not, give out 2 penalties.",
+      ],
+      [
+        "",
+        ", if you ever blacked out at Joes, take 2 penalties. If not, give out 2 penalties.",
+      ],
+      ["Computer Science majors take 2 penalties."],
+      [
+        "",
+        ", if you ever went to Cracked hungover, take 2 penalties. Otherwise, give out 2 penalties.",
+      ],
+      [
+        "",
+        ", if you ever took a walk on Green while wasted, take 3 penalties.",
+      ],
+      ["If you ever participated in Unofficial, take 1 penalty."],
+      [
+        "",
+        ", if you haven't drank in the past two weeks, give out a maximum penalty.",
+      ],
     ],
   },
 ];
